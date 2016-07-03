67cbc6e3053e89e175bb7bfb7840d280ddc56a74
Antoni Dragomanov <adragomanov@hhogdev.com>
2016-07-03 19:05:25 +0300

    * Added renderLayout pipeline processor for inserting the js code on the pages

---
+++ SitecoreCookieConsent/Pipelines/RenderLayout/InsertCookieConsent.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Web.UI;
+using Sitecore.CookieConsent.layouts.Modules.CookieConsent;
+using Sitecore.CookieConsent.Models;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Pipelines.RenderLayout;
+
+namespace Sitecore.CookieConsent.Pipelines.RenderLayout
+{
+    public class InsertCookieConsent : RenderLayoutProcessor
+    {
+        public override void Process(RenderLayoutArgs args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            InsertControl();
+            sw.Stop();
+            Diagnostics.Log.Info($"Cookie Consent took {sw.ElapsedMilliseconds}ms", this);
+        }
+
+        protected virtual void InsertControl()
+        {
+            string settingsItemPath = $"/sitecore/system/Modules/Cookie Consent/Settings/{Context.GetSiteName()}";
+            Item settingItem = Context.Database?.GetItem(settingsItemPath);
+
+            if (settingItem == null)
+            {
+                return;
+            }
+
+            Settings model = new Settings
+            {
+                Enabled = settingItem["Enabled"] == "1",
+                Message = settingItem["Message"],
+                DismissButton = settingItem["DismissButton"],
+                LearnMore = settingItem["LearnMore"],
+                PolicyLink = ((LinkField)settingItem.Fields["PolicyLink"])?.GetFriendlyUrl(),
+                Theme = settingItem["Theme"]
+            };
+
+            if (!model.Enabled)
+            {
+                return;
+            }
+
+            Page page = Context.Page?.Page;
+            ScriptJs control = (ScriptJs)page?.LoadControl("~/layouts/Modules/CookieConsent/ScriptJs.ascx");
+
+            if (control == null)
+            {
+                return;
+            }
+
+            control.Model = model;
+            page?.Controls.Add(control);
+        }
+    }
+}
--- SitecoreCookieConsent/layouts/Modules/CookieConsent/ScriptJs.ascx
+++ SitecoreCookieConsent/layouts/Modules/CookieConsent/ScriptJs.ascx
@@ -3,13 +3,13 @@
 <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
 <script type="text/javascript">
     window.cookieconsent_options = {
-        "message": <%= Model.Message %>,
-        "dismiss": <%= Model.DismissButton %>,
-        "learnMore": <%= Model.LearnMore %>,
-        "link": <%= Model.PolicyLink %>,
-        "theme": <%= Model.Theme %>
+        "message": "<%= Model.Message %>",
+        "dismiss": "<%= Model.DismissButton %>",
+        "learnMore": "<%= Model.LearnMore %>",
+        "link": "<%= Model.PolicyLink %>",
+        "theme": "<%= Model.Theme %>"
     };
 </script>
 
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js" async></script>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
 <!-- End Cookie Consent plugin -->

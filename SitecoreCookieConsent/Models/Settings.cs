6d4f405a6f8bad85da69af1974fd218427aeb155
Antoni Dragomanov <adragomanov@hhogdev.com>
2016-07-03 15:08:00 +0300

    * Added model for settings

---
+++ SitecoreCookieConsent/Models/Settings.cs
@@ -0,0 +1,17 @@
+﻿namespace SitecoreCookieConsent.Models
+{
+    public class Settings
+    {
+        public bool Enabled { get; set; }
+
+        public string DismissButton { get; set; }
+
+        public string LearnMore { get; set; }
+
+        public string Message { get; set; }
+
+        public string PolicyLink { get; set; }
+
+        public string Theme { get; set; }
+    }
+}

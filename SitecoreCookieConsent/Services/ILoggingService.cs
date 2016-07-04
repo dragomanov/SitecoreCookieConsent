789cdbaabc52ff77b770fb46c2e5b77b6b4b83b7
Antoni Dragomanov <adragomanov@hhogdev.com>
2016-07-04 17:50:22 +0300

    * Implemented LoggingService for logging

---
+++ SitecoreCookieConsent/Services/ILoggingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sitecore.CookieConsent.Services
+{
+    public interface ILoggingService
+    {
+        void LogDebug(string message);
+
+        void LogInfo(string message);
+
+        void LogWarn(string message);
+
+        void LogError(string message);
+
+        void LogError(string message, Exception ex);
+
+        void LogFatal(string message);
+
+        void LogFatal(string message, Exception ex);
+    }
+}
---
+++ SitecoreCookieConsent/Services/LoggingService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Sitecore.Diagnostics;
+
+namespace Sitecore.CookieConsent.Services
+{
+    public class LoggingService : ILoggingService
+    {
+        public void LogDebug(string message)
+        {
+            Log.Debug(message, typeof(LoggingService));
+        }
+
+        public void LogInfo(string message)
+        {
+            Log.Info(message, typeof(LoggingService));
+        }
+
+        public void LogWarn(string message)
+        {
+            Log.Warn(message, typeof(LoggingService));
+        }
+
+        public void LogError(string message)
+        {
+            LogError(message, null);
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            Log.Error(message, ex, typeof(LoggingService));
+        }
+
+        public void LogFatal(string message)
+        {
+            LogFatal(message, null);
+        }
+
+        public void LogFatal(string message, Exception ex)
+        {
+            Log.Fatal(message, ex, typeof(LoggingService));
+        }
+    }
+}

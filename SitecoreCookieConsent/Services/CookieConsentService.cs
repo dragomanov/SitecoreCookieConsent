5d54a6a7960991a2c012a53e06da47aa29c16ce6
Antoni Dragomanov <adragomanov@hhogdev.com>
2016-07-10 12:59:51 +0300

    * GetSettingsModel now uses field IDs

--- SitecoreCookieConsent/Services/CookieConsentService.cs
+++ SitecoreCookieConsent/Services/CookieConsentService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web.UI;
 using Sitecore.CookieConsent.layouts.Modules.CookieConsent;
 using Sitecore.CookieConsent.Models;
+using Sitecore.Data;
 using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
 
@@ -9,6 +10,13 @@ namespace Sitecore.CookieConsent.Services
     public class CookieConsentService : ICookieConsentService
     {
         private const string SettingsItemPath = "/sitecore/system/Modules/Cookie Consent/Settings/{0}";
+
+        private static readonly ID EnabledFieldID = new ID("{48458386-3B70-414E-BC99-9545D41FA78F}");
+        private static readonly ID MessageFieldID = new ID("{6B7B8503-E07F-43F1-B7E6-C0CBD2332274}");
+        private static readonly ID DismissButtonFieldID = new ID("{13157F2A-E4AA-432E-A2A5-2403103AD1E2}");
+        private static readonly ID LearnMoreFieldID = new ID("{8662EF97-1FBD-46DB-B70E-22826CC1C313}");
+        private static readonly ID PolicyLinkFieldID = new ID("{14A55E2B-F047-4D11-AA71-F235C06CE341}");
+        private static readonly ID ThemeFieldID = new ID("{3205FD36-5762-4F45-A0B1-C206110E4479}");
 
         public void RenderControl()
         {
@@ -38,16 +46,16 @@ namespace Sitecore.CookieConsent.Services
 
         private static CookieConsentSettings GetSettingsModel(Item settingItem)
         {
-            var policyLink = (LinkField)settingItem.Fields["PolicyLink"];
+            var policyLink = (LinkField)settingItem.Fields[PolicyLinkFieldID];
 
             return new CookieConsentSettings
             {
-                Enabled = settingItem["Enabled"] == "1",
-                Message = settingItem["Message"],
-                DismissButton = settingItem["DismissButton"],
-                LearnMore = settingItem["LearnMore"],
+                Enabled = settingItem[EnabledFieldID] == "1",
+                Message = settingItem[MessageFieldID],
+                DismissButton = settingItem[DismissButtonFieldID],
+                LearnMore = settingItem[LearnMoreFieldID],
                 PolicyLink = policyLink != null ? policyLink.GetFriendlyUrl() : string.Empty,
-                Theme = settingItem["Theme"]
+                Theme = settingItem[ThemeFieldID]
             };
         }
 
